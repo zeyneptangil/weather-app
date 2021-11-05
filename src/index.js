51a79ade56daff73055ac5cae5bcf0b7aeb04b4a
zeyneptangil <91064741+zeyneptangil@users.noreply.github.com>
2021-11-05 14:02:35 +0000

    Added weather icon from API

--- src/index.js
+++ src/index.js
@@ -37,6 +37,14 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
